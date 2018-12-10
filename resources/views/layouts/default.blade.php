3e16ca3f3265b9842d97a106a718c2b8215ae4e3
yomi2201 <brbr091429@yahoo.co.jp>
2018-12-11 03:57:01 +0900

    end delete action

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>@yield('title')</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <div class="container">
+    @yield('content')
+  </div>
+  <script src='js/main.js'></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/want_to_do',function(){
-    return view('want');
-});
+Route::get('/', 'PostsController@index');
+// Route::get('/posts/{id}', 'PostsController@show');
+Route::get('/posts/{post}', 'PostsController@show')->where('post', '[0-9]+');
+Route::get('/posts/create', 'PostsController@create');
+Route::post('/posts', 'PostsController@store');
+Route::get('/posts/{post}/edit', 'PostsController@edit');
+Route::patch('/posts/{post}', 'PostsController@update');
+Route::delete('/posts/{post}', 'PostsController@destroy');
